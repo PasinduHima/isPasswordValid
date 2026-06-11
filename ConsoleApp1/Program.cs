d3e5c9eab84b0fee0d888f6db50a907e20f99b6d
Pasindu <himalpasindu01@gmail.com>
2026-06-11 06:19:38 +0530

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace myApplication;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int sum = 0;
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+            sum += i;
+        }
+        Console.WriteLine("Sum of total tanural numbers : {0}",sum);
+        Console.ReadLine();
+    }
+}
+
+  
